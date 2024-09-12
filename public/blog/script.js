ef1e8550841cdb56a711f516e73d5c1d53763185
Kameron <cursedgreats@gmail.com>
2024-09-12 15:58:19 -0300

    add(Blog): Browsers Blog Post
    it's done!

--- public/blog/script.js
+++ public/blog/script.js
@@ -33,7 +33,7 @@ let authorLink = "https://kam106.neocities.org"; // Enter your website, social m
 let postsArray = [
 //[ "posts/2020-11-10-Special-Characters-Example.html", encodeURI( 'Spéci@l "Character\'s" Examp|e' ) ],
 //[ "posts/2020-11-10-My-Third-Post-Example.html" ],
-//[ "posts/2024-09-10-Browsers-and-Other-Floorp.html", encodeURI( 'Browsers & Other Floorp: My History with Browsing' ) ], -- it's not ready yet oops
+[ "posts/2024-09-12-Browsers-and-Other-Floorp.html", encodeURI( 'Browsers & Other Floorp: My History with Browsing' ) ],
 [ "posts/2024-09-08-Switching-to-Neocities.html", encodeURI( 'Switching to Neocities: The New Life' ) ],
 [ "posts/2024-09-07-Sky-Parody-Blog.html", encodeURI( 'Mystery Recipe to Try!!! From Undertale Yellow (Recipe and Review) (GOT ARRESTED)' ) ],
 [ "posts/2024-09-04-Last-Online.html", encodeURI( 'Last Online: The Ghosts of the Internet' ) ],
